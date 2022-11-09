9a49f8599313a3d4db58b7864fcd1c19f54de52a
56u63 <antoine.boury1@gmail.com>
2022-11-09 07:49:45 +0100

    deuxième session de travail

--- ex3.py
+++ ex3.py
@@ -1,11 +1,12 @@
 from tkinter import *
 from PIL import Image,ImageTk
- 
+from random import randint
 
 
 root=Tk()
 
 ###############################création du damier##########################################
+global size
 size=50
 main_canva=Canvas(root,bg="white",width=400,height=400)
 def white_start(i):
@@ -30,26 +31,66 @@ for i in range(8):
         black_start(i)
 
 
-#############################importation de l'image##########################################
-img= ImageTk.PhotoImage(Image.open("dame_resize.png"))
-dame=main_canva.create_image(25,25,image=img)
+###############################création de la classe dame######################################
+class queen():
+    def __init__(self,pos_queen):
+        self.img=ImageTk.PhotoImage(Image.open("dame_resize.png"))
+        a=randint(0,7)
+        b=randint(0,7)
+        while [a,b] in pos_queen:
+            a=randint(0,7)
+            b=randint(0,7)
+        self.pos_x=a
+        self.pos_y=b
+        pos_queen.append([self.pos_x,self.pos_y])
 
-###############################déplacement de la dame######################################
-def left(e):
-    main_canva.move(dame,-size,0)
-def right(e):
-    main_canva.move(dame,size,0)
-def down(e):
-    main_canva.move(dame,0,size)
-def up(e):
-    main_canva.move(dame,0,-size)
+    def create_queen(self,size):
+        pos_x=self.pos_x*size+size/2
+        pos_y=self.pos_y*size+size/2
+        self.queen=main_canva.create_image(pos_x,pos_y,image=self.img)
+#################################intialisation des 8 dames#####################################
 
+def create_8_queens(size):
+    pos_queen=[]
+    queens=[]
+    for i in range(8):
+        current_queen=queen(pos_queen)
+        current_queen.create_queen(size)
+        queens.append(current_queen)
+    return pos_queen,queens
 
-root.bind("<Right>",right)
-root.bind("<Left>",left)
-root.bind("<Down>",down)
-root.bind("<Up>",up)
+global queen_pos
+global queens
+queens_list=create_8_queens(size)
+queen_pos=queens_list[0]
+queens=queens_list[1]
+###################################callbacks events############################################
+def mouse_pos_checker(e):
+    global queen_pos
+    global size
+    pos=[int((e.x-(e.x%size))/size),int((e.y-(e.y%size))/size)]
+    out=8
+    for i in range(len(queen_pos)):
+        if pos==queen_pos[i]:
+            out=i
+        else:
+            pass
+    return out
+def move(e):
+    mouse_case=mouse_pos_checker(e)
+    if mouse_case==8:
+        pass
+    else:
+        global queens
+        queens[mouse_case].queen.destroy()
+        queens[mouse_case].queen=main_canva.create_image(e.x,e.y,image=queens[mouse_case].img)
 
+def release(e):
+    print("")
+label=Label(root,text="")
+label.pack()
 
+root.bind("<B1-Motion>",move)
+root.bind("<ButtonRelease-1>",release)
 main_canva.pack()
 root.mainloop()
