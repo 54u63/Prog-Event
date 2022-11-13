2afca49a63d12a7394027b8f7fba9cf4ba281b45
56u63 <antoine.boury1@gmail.com>
2022-11-13 23:25:02 +0100

    commit d'échéance

--- ex3_new.py
+++ ex3_new.py
@@ -32,12 +32,15 @@ for i in range(8):
 
 
 ###############################création de la classe dame######################################
+img=ImageTk.PhotoImage(Image.open("dame_resize.png"))
+
 class queen():
-    def __init__(self,pos_queen):
+    def __init__(self,pos_queen,img):
         self.show_case=False
-        self.img=ImageTk.PhotoImage(Image.open("dame_resize.png"))
+        self.img=img
         a=randint(0,7)
         b=randint(0,7)
+        self.id=0
         while [a,b] in pos_queen:
             a=randint(0,7)
             b=randint(0,7)
@@ -113,12 +116,13 @@ class queen():
 
 
 def create_8_queens(size):
-    pos_queen=[]
+    pos_queen=[] 
     queens=[]
     for i in range(8):
-        current_queen=queen(pos_queen)
+        current_queen=queen(pos_queen,img)
         current_queen.create_queen(size)
         current_queen.aviable_case()
+        current_queen.id=i
         queens.append(current_queen)
     return pos_queen,queens
 
@@ -131,8 +135,9 @@ queens=queens_list[1]
 
 ###############################fonctions########################################################
 
-def color_aviable(queen,pos_queens,size,color_tab=[]):
+def color_aviable(queen,pos_queens,size,e,color_tab=[]):
     aviablepos=queen.case
+
     out=1
     if queen.show_case==False:
         color_tab=[]
@@ -154,16 +159,39 @@ def color_aviable(queen,pos_queens,size,color_tab=[]):
         return True,color_tab
 
     if queen.show_case==True:
+        mouse_case=[int((e.x-(e.x%size))/size),int((e.y-(e.y)%size)/size)]
+        for i in range(len(queen.case)):
+            try:                        #########################FIX ME##################
+                if queen.case[i]==mouse_case:
+                    queen.case.pop(i)
+            except IndexError:
+                pass                    
         for i in range(len(aviablepos)):
             posx_square=aviablepos[i][0]*size
             posy_square=aviablepos[i][1]*size
             if aviablepos[i] in pos_queens:
                 main_canva.create_image(posx_square+size/2,posy_square+size/2,image=queen.img)
             else:
-                print(color_tab[i])
                 main_canva.create_rectangle(posx_square,posy_square,posx_square+size,posy_square+size,fill=color_tab[i])
         return False,color_tab
 
+def move_queen(queen,e):
+    global size
+    global color_tab
+    mouse_case=[int((e.x-(e.x%size))/size),int((e.y-(e.y)%size)/size)]
+    if mouse_case in queen.case:
+        pos=0
+        for i in range(len(queen.case)):
+            if mouse_case==queen.case:
+                pos+=i
+        if color_tab[pos]=="queen":
+            print("salam")
+            return 1
+        print(mouse_case)
+        move=[mouse_case[0]*size+size/2,mouse_case[1]*size+size/2]
+        main_canva.create_image(move[0],move[1],image=img)
+    else:
+        print("bye")
 
 
 ###################################callbacks events############################################
@@ -181,23 +209,24 @@ def mouse_pos_checker(e):
 
 global color_tab
 color_tab=[]
+global cu_queen
+cu_queen=queens[0]
 def move(e):
     mouse_case=mouse_pos_checker(e)
-    if mouse_case==8:
-        pass
+    global queens
+    global queen_pos
+    global size
+    global color_tab
+    global cu_queen
+    if mouse_case<8 and queens[mouse_case].show_case==False:
+        cu_queen=queens[mouse_case]
+        out=color_aviable(cu_queen,queen_pos,size,e)
+        cu_queen.show_case=out[0]
+        color_tab=out[1]
     else:
-        global queens
-        global queen_pos
-        global size
-        global color_tab
-        if queens[mouse_case].show_case==False:
-            out=color_aviable(queens[mouse_case],queen_pos,size)
-            queens[mouse_case].show_case=out[0]
-            color_tab=out[1]
-        else:
-            out=color_aviable(queens[mouse_case],queen_pos,size,color_tab=color_tab)
-            queens[mouse_case].show_case=out[0]
-            print(out[1])
+        move_queen(cu_queen,e)
+        out=color_aviable(cu_queen,queen_pos,size,e,color_tab=color_tab)
+        cu_queen.show_case=out[0]
 
 
 def release(e):
