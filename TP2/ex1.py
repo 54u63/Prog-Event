7f68bb775293443396e3d060e55bad7bc400238e
56u63 <antoine.boury1@gmail.com>
2022-11-16 15:52:35 +0100

    importaiton des image et création des objets routeurs et pointeurs

--- TP2/ex1.py
+++ TP2/ex1.py
@@ -1,8 +1,27 @@
 from tkinter import *
-
+from PIL import Image,ImageTk
 root=Tk()
 
 
-test_label=Label(root,text="hello world")
-test_label.pack()
+main_canva=Canvas(root,bg="white",width=1000,height=1000)
+
+class pointeur:
+    def __init__(self):
+        self.state="None"
+
+class routeur:
+    def __init___(self):
+        self.img=ImageTk.PhotoImage(Image.open("routeur.jpg"))
+        self.posx=0
+        self.posy=0
+    def initialize(self,e,identifier):
+        self.posx=e.x
+        self.posy=e.y
+        self.id=identifier
+        self.routeur=main_canva.create_image(posx,posy,image=self.img)
+
+
+selector=pointeur()
+
+
 root.mainloop()
