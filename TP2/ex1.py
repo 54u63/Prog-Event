13557c4d796ee0b3a3dd2d128f38503497dadf24
56u63 <antoine.boury1@gmail.com>
2022-11-16 16:13:01 +0100

    ajout des deux classes switchs et clients

--- TP2/ex1.py
+++ TP2/ex1.py
@@ -13,15 +13,41 @@ class routeur:
     def __init___(self):
         self.img=ImageTk.PhotoImage(Image.open("routeur.jpg"))
         self.posx=0
+        self.posy=0 
+    def initialize(self,e,identifier):
+        self.posx=e.x
+        self.posy=e.y
+        self.id=identifier
+        self.routeur=main_canva.create_image(self.posx,self.posy,image=self.img)
+
+
+class switch:
+    def __init__(self):
+        self.img=ImageTk.PhotoImage(Image.open("switch.png"))
+        self.posx=0
         self.posy=0
     def initialize(self,e,identifier):
         self.posx=e.x
         self.posy=e.y
         self.id=identifier
-        self.routeur=main_canva.create_image(posx,posy,image=self.img)
+        self.switch=main_canva.create_image(self.posx,self.posy,image=self.img)
+
+class client:
+    def __init__(self):
+        self.img=ImageTk.PhotoImage(Image.open("PC.png"))
+        self.posx=0
+        self.posy=0
+    def initialize(self,e,identifier):
+        self.posx=e.x
+        self.posy=e.y
+        self.id=identifier
+        self.client=main_canva.createimage(self.posx,self.posy,image=self.img)
+
+
 
 
 selector=pointeur()
+
 
 def pressed(e):
     if e.char=="r":
@@ -32,7 +58,7 @@ def pressed(e):
         selector.state="Client"
     else:
         pass
-    print(e.char)
+
 
 root.bind("<KeyPress>",pressed)
 
