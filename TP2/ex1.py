47a1ee86ce6fb0ad3f099c15b844188f2cd20d9c
56u63 <antoine.boury1@gmail.com>
2022-11-20 21:21:11 +0100

    ajout de menu click droit avec icone et rename

--- TP2/ex1.py
+++ TP2/ex1.py
@@ -1,5 +1,6 @@
 from tkinter import *
 from PIL import Image,ImageTk
+from time import *
 root=Tk()
 
 
@@ -11,7 +12,7 @@ main_canva.pack()
 class pointeur:
     def __init__(self):
         self.state="None"
-
+        self.menu_enable=False
 
 
 ########################################CREATION DE L'OBJET ROUTEUR#########################
@@ -49,8 +50,7 @@ class switch:
         et créé positions
         """
         self.img=ImageTk.PhotoImage(Image.open("switch.png"))
-        self.posx=0
-        self.posy=0
+        self.size=32.5
     def initialize(self,e,identifier):
         """
         initialise l'objet en l'important dans le canva
@@ -58,9 +58,12 @@ class switch:
         """
         self.posx=e.x
         self.posy=e.y
+        self.rangeX=[self.posx-self.size,self.posx+self.size]
+        self.rangeY=[self.posx-self.size,self.posy+self.size]       
         self.id=identifier
         self.name="Sw"+str(self.id)
         self.switch=main_canva.create_image(self.posx,self.posy,image=self.img)
+        self.placeholder=main_canva.create_text(self.posx,self.posy+self.size,text=self.name)
 
 
 ##########################################CREATION DE LA CLASSE CLIENT#########################
@@ -72,8 +75,7 @@ class client:
         et créé positions
         """       
         self.img=ImageTk.PhotoImage(Image.open("PC.png"))
-        self.posx=0
-        self.posy=0
+        self.size=32.5
     def initialize(self,e,identifier):
         """
         initialise l'objet en l'important dans le canva
@@ -81,9 +83,41 @@ class client:
         """
         self.posx=e.x
         self.posy=e.y
+        self.rangeX=[self.posx-self.size,self.posx+self.size]
+        self.rangeY=[self.posx-self.size,self.posy+self.size]        
         self.id=identifier
         self.name="C"+str(self.id)
         self.client=main_canva.create_image(self.posx,self.posy,image=self.img)
+        self.placeholder=main_canva.create_text(self.posx,self.posy+self.size,text=self.name)
+
+class menu_square:
+    def __init__(self):
+        self.size_x=100
+        self.size_y=50
+        self.text_1="renomer"
+        self.text_2="icone"
+    def init_menu(self,e):
+        self.t1x=e.x+27
+        self.t2x=e.x+18
+        self.t1y=e.y+7
+        self.t2y=e.y+20 
+        self.rect=main_canva.create_rectangle(e.x,e.y,e.x+self.size_x,e.y+self.size_y,outline="black",fill="white")
+        self.t1=main_canva.create_text(self.t1x,self.t1y,text=self.text_1)
+        self.t2=main_canva.create_text(self.t2x,self.t2y,text=self.text_2)
+    def destroy_menu(self):
+        main_canva.delete(self.rect)
+        main_canva.delete(self.t1)
+        main_canva.delete(self.t2)
+    def is_clicked(self,e):
+        if e.x<self.t1x+(self.size_x-27) and e.x>self.t1x-27:
+            if e.y<self.t1y+7 and e.y>self.t1y-7:
+                print("rename")
+                return 0
+        if e.x<self.t2x+(self.size_x-18) and e.x>self.t2x-18:
+            print("hi")
+            if e.y<self.t2y+7 and e.x>self.t2y-7:
+                print("icone")
+                return 0
 
 #########################################FONCTIONS####################################
 
@@ -102,7 +136,18 @@ def create_client(e,object_list):
     new_client.initialize(e,len(object_list[2]))
     object_list[2].append(new_client)
 
+def menu(e):
+    global menu_clicked
+    menu_clicked=menu_square()
+    menu_clicked.init_menu(e)
+    selector.menu_enable=True
+    return [e.x,e.y,e.x+100,e.y+50]
 
+
+def destroy_menu():
+    global menu_clicked
+    menu_clicked.destroy_menu()
+    selector.menu_enable=False
 
 
 ########################################VARIABLES####################################
@@ -121,8 +166,12 @@ def click(e):
         create_switch(e,object_list)
     elif selector.state=="Client":
         create_client(e,object_list)
+    elif selector.menu_enable==True and selector.state=="None":
+        global menu_clicked
+        menu_clicked.is_clicked(e)
     else:
         pass
+        
 
 
 def pressed(e):
@@ -137,7 +186,25 @@ def pressed(e):
     else:
         pass
 
-
+def rmb(e):
+    global object_list
+    test=0
+    if selector.menu_enable is False:
+        for i in range(len(object_list)):
+            for j in range(len(object_list[i])):
+                print(str(e.x))
+                print(str(e.y))
+                print(str(object_list[i][j].rangeX)+" "+str(object_list[i][j].rangeY))
+                if e.x < object_list[i][j].rangeX[1] and e.x > object_list[i][j].rangeX[0]:
+                    if e.y<object_list[i][j].rangeY[1] and e.y > object_list[i][j].rangeY[0]:
+                        global menu_size
+                        menu_size=menu(e)
+                        return 0
+    if selector.menu_enable is True:
+        destroy_menu()    
+   
 root.bind("<KeyPress>",pressed)
 root.bind("<Button-1>",click)
+root.bind("<Button-2>",rmb)
+root.bind("<Button-3>",rmb)
 root.mainloop()
