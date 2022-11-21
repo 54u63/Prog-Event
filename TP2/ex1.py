4554f5194ac123de9b4cf3350f7716b5bcd94209
56u63 <antoine.boury1@gmail.com>
2022-11-21 20:46:07 +0100

    changement de nom et d'icone possible

--- TP2/ex1.py
+++ TP2/ex1.py
@@ -16,6 +16,7 @@ class pointeur:
     def __init__(self):
         self.state="None"
         self.menu_enable=False
+        self.icone=False
         self.obj=[0,0]
 
 ########################################CREATION DE L'OBJET ROUTEUR#########################
@@ -44,43 +45,37 @@ class equipement():
         main_canva.itemconfigure(self.placeholder,text=self.name)
 
 class routeur(equipement):
-    def __init__(se lf):
+    def __init__(self,images):
         super().__init__()
-        self.img=ImageTk.PhotoImage(Image.open("routeur.png"))
         self.prefix="R"
+        self.img=images[0]
     def initialize(self,e,identifier):
         super().initialize(e,identifier)
     def change_name(self,name):
         super().change_name(name)
-
-
 ########################################CREATION DE LA CLASSE SWITCH###########################
 class switch(equipement):
-    def __init__(self):
+    def __init__(self,images):
         super().__init__()
-        self.img=ImageTk.PhotoImage(Image.open("switch.png"))
         self.prefix="S"
+        self.img=images[1]
     def initialize(self,e,identifier):
         super().initialize(e,identifier)
     def change_name(self,name):
         super().change_name(name)
-
-
 #########################################CREATION DE LA CLASSE CLIENT#########################
-
 class client(equipement):
-    def __init__(self):
+    def __init__(self,images):
         super().__init__()
-        self.img=ImageTk.PhotoImage(Image.open("PC.png"))
         self.prefix="C"
+        self.img=images[2]
     def initialize(self,e,identifier):
         super().initialize(e,identifier)
     def change_name(self,name):
         super().change_name(name)
-
 
 class menu_square:
-    def __init__(self):
+    def __init__(self): 
         self.size_x=100
         self.size_y=50
         self.text_1="renomer"
@@ -104,12 +99,10 @@ class menu_square:
                 rename(object_list[selector.obj[0]][selector.obj[1]].name)
                 return 0
         if e.x<self.t2x+(self.size_x-18) and e.x>self.t2x-18:
-            if e.y<self.t2y+7 and e.x>self.t2y-7:
-                print("icone")
+            if e.y<self.t2y+7 and e.y>self.t2y-7:
+                icone()
                 return 0
-
 #########################################FONCTIONS####################################
-
 def rename(name):
     global object_list
     obj=object_list[selector.obj[0]][selector.obj[1]]
@@ -127,32 +120,48 @@ def rename(name):
     bouton=ttk.Button(root,text="valider",command=get_name)
     cute_labe=main_canva.create_window(500,400,window=label)
     cute_txtbox=main_canva.create_window(500,425,window=textbox)
-    cut e_button=main_canva.create_window(500,450,window=bouton)
+    cute_button=main_canva.create_window(500,450,window=bouton)
 
 def icone():
-    cute_rectangle=main_canva.create_rectangle(200,200,800,800,outline="black",fill="white")
-    switch=main_canva.create_image(self.posx,self.posy,image="switch.png")
-    routeur=main_canva.create_image(self.posx,self.posy,image="routeur.png")
-    client=main_canva.create_image(self.posx,self.posy,image="PC.png")
+    print("hi")
+    global object_list
+    obj=object_list[selector.obj[0]][selector.obj[1]]
+    global images
+    pref_list=["R","S","C"]
+    index=pref_list.index(obj.prefix)
+    pref_used=""
+    if index==len(pref_list)-1:
+        pref_used+=pref_list[0]
+    else:
+        pref_used+=pref_list[index+1]
+    if pref_used=="R":
+        main_canva.itemconfigure(obj.object,image=images[0])
+    if pref_used=="C":
+        main_canva.itemconfigure(obj.object,image=images[2])
+    if pref_used=="S":
+        main_canva.itemconfigure(obj.object,image=images[1])
+    else:
+        pass
+    global menu_clicked
+    menu_clicked.destroy_menu()
+    obj.prefix=pref_used
 
 
-
-
-def create_routeur(e,object_list):
-    selector.state="None"
-    new_routeur=routeur()
+def create_routeur(e,object_list,images):
+    selector.state="None" 
+    new_routeur=routeur(images)
     new_routeur.initialize(e,len(object_list[0]))
-    object_list[0].append(new_routeur)
+    object_list[0].append(new_routeur) 
     
-def create_switch(e,object_list):
-    selector.state="None"
-    new_switch=switch() 
+def create_switch(e,object_list,images):
+    selector.state="None" 
+    new_switch=switch(images)
     new_switch.initialize(e,len(object_list[1]))
     object_list[1].append(new_switch)
 
-def create_client(e,object_list):
-    selector.state="None"
-    new_client=client()
+def create_client(e,object_list,images):
+    selector.state="None" 
+    new_client=client(images)
     new_client.initialize(e,len(object_list[2]))
     object_list[2].append(new_client)
 
@@ -161,31 +170,32 @@ def menu(e):
     menu_clicked=menu_square()
     menu_clicked.init_menu(e)
     selector.menu_enable=True
-    return [e.x,e.y,e.x+100,e.y+50]
-
+    return [e.x,e.y,e.x+ 100,e.y+50]
 
 def destroy_menu():
-    global menu_clicked
+    global menu_clicked 
     menu_clicked.destroy_menu()
     selector.menu_enable=False 
-
-
 ########################################VARIABLES####################################
 selector=pointeur()
 global object_list
 object_list=[[],[],[]]
 exit_button=Button(root,text="X",command=root.quit())
 main_canva.create_window(925,10,window=exit_button)
+global images
+images=[ImageTk.PhotoImage(Image.open("routeur.png")),
+        ImageTk.PhotoImage(Image.open("switch.png")),
+        ImageTk.PhotoImage(Image.open("PC.png"))]
 
 #########################################CALLBACKS###############################
 def click(e):
     global object_list
     if selector.state=="Routeur":
-        create_routeur(e,object_list)
+        create_routeur(e,object_list,images)
     elif selector.state=="Switch":
-        create_switch(e,object_list)
+        create_switch(e,object_list,images)
     elif selector.state=="Client":
-        create_client(e,object_list)
+        create_client(e,object_list,images)
     elif selector.menu_enable==True and selector.state=="None":
         global menu_clicked
         menu_clicked.is_clicked(e)
